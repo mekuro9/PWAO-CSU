ea8e55c8a93124ee7dbcc8ae043c525c0241df4b
mekuro9 <60172980+mekuro9@users.noreply.github.com>
2024-04-09 14:52:00 +0200

    Add files via upload

--- humble_ws/src/sequence_controller/src/sequence_controller.cpp
+++ humble_ws/src/sequence_controller/src/sequence_controller.cpp
@@ -1,5 +1,5 @@
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/float64.hpp"
+#include "std_msgs/msg/float32.hpp"
 #include "std_msgs/msg/int32.hpp"
 #include "std_msgs/msg/bool.hpp"
 #include "std_msgs/msg/string.hpp"
@@ -20,30 +20,28 @@ public:
     SequenceController() 
         : Node("sequence_controller"), 
           state_(WheelchairState::Stopped), 
-          forward(false), turn(0), current_angle_(0.0), angle_error_(0.0), 
-          current_velocity_(0.0), velocity_error_(0.0),is_forward_timer_active_(false)  {
+          state(0), turn(0), current_angle_(0.0), angle_error_(0.0), 
+          relative_angle_(0.0),is_forward_timer_active_(false)  {
         
         command_subscriber_ = this->create_subscription<std_msgs::msg::String>(
         "/webinput/command", 1, std::bind(&SequenceController::webinput_callback, this, _1));
 
-        current_angle_subscriber_ = this->create_subscription<std_msgs::msg::Float64>(
+        current_angle_subscriber_ = this->create_subscription<std_msgs::msg::Float32>(
             "loop/currentAngle", 1, std::bind(&SequenceController::currentAngleCallback, this, _1));
 
-        angle_error_subscriber_ = this->create_subscription<std_msgs::msg::Float64>(
+        angle_error_subscriber_ = this->create_subscription<std_msgs::msg::Float32>(
             "loop/angleError", 1, std::bind(&SequenceController::angleErrorCallback, this, _1));
 
-        current_velocity_subscriber_ = this->create_subscription<std_msgs::msg::Float64>(
-            "loop/currentSpeed", 1, std::bind(&SequenceController::currentVelocityCallback, this, _1));
+        relative_angle_subscriber_ = this->create_subscription<std_msgs::msg::Float32>(
+            "loop/relativeAngle", 1, std::bind(&SequenceController::relativeAngleCallback, this, _1));
 
-        velocity_error_subscriber_ = this->create_subscription<std_msgs::msg::Float64>(
-            "loop/speedError", 1, std::bind(&SequenceController::velocityErrorCallback, this, _1));
-
-        speed_publisher_ = this->create_publisher<std_msgs::msg::Bool>("setpoint/speed",1);
+        
+        state_publisher_ = this->create_publisher<std_msgs::msg::Int32>("setpoint/state",1);
         angle_publisher_ = this->create_publisher<std_msgs::msg::Int32>("setpoint/angle",1);        
 
 
         timer_ = this->create_wall_timer(
-            100ms, std::bind(&SequenceController::timer_callback,this)
+            10ms, std::bind(&SequenceController::timer_callback,this)
         );
 
         
@@ -63,21 +61,18 @@ private:
         }
     }
 
-    void currentAngleCallback(const std_msgs::msg::Float64::SharedPtr msg) {
+    void currentAngleCallback(const std_msgs::msg::Float32::SharedPtr msg) {
         current_angle_ = msg->data;
     }
 
-    void angleErrorCallback(const std_msgs::msg::Float64::SharedPtr msg) {
+    void angleErrorCallback(const std_msgs::msg::Float32::SharedPtr msg) {
         angle_error_ = msg->data;
     }
 
-    void currentVelocityCallback(const std_msgs::msg::Float64::SharedPtr msg) {
-        current_velocity_ = msg->data;
+    void relativeAngleCallback(const std_msgs::msg::Float32::SharedPtr msg) {
+        relative_angle_ = msg->data;
     }
 
-    void velocityErrorCallback(const std_msgs::msg::Float64::SharedPtr msg) {
-        velocity_error_ = msg->data;
-    }
 
     void transitionToState(WheelchairState new_state) {
         // Ignore new commands if the forward timer is active
@@ -89,47 +84,49 @@ private:
         switch (new_state) {
             case WheelchairState::MovingForward:
                 if (state_ != WheelchairState::MovingForward) {
-                    forward = true;
-                    turn = 0;
+                    state = 1;
+                    turn += 0;
                     is_forward_timer_active_ = true;
+
+                    RCLCPP_INFO(this->get_logger(), "I am in Forward: '%s'", std::to_string(state));
+                    
                     speed_duration_timer_ = this->create_wall_timer(
-                        3s, [this]() {
+                        2s, [this]() {
                             transitionToState(WheelchairState::Stopped);
                             is_forward_timer_active_ = false;
                         });
                 }
-                break;
-
+            break;
             case WheelchairState::TurningRight:
                 // Stop the forward timer if it's running
                 if (is_forward_timer_active_) {
                     speed_duration_timer_->cancel();
                     is_forward_timer_active_ = false;
                 }
-                forward = false;
-                turn = 1;
-                break;
-
+                state = 2;
+                RCLCPP_INFO(this->get_logger(), "I am in Right: '%s'", std::to_string(state));
+                turn += 1*rotation_step;
+            break;
+            case WheelchairState::Stopped:
+                if (is_forward_timer_active_) {
+                    speed_duration_timer_->cancel();
+                    is_forward_timer_active_ = false;
+                }
+                state = 0;
+                RCLCPP_INFO(this->get_logger(), "I am in Stopped: '%s'", std::to_string(state));
+                turn += 0;
+            break;
             case WheelchairState::TurningLeft:
                 // Stop the forward timer if it's running
                 if (is_forward_timer_active_) {
                     speed_duration_timer_->cancel();
                     is_forward_timer_active_ = false;
                 }
-                forward = false;
-                turn = -1;
-                break;
-
-            case WheelchairState::Stopped:
-            default:
-                // Cancel the forward timer if it's running
-                if (is_forward_timer_active_) {
-                    speed_duration_timer_->cancel();
-                    is_forward_timer_active_ = false;
-                }
-                forward = false;
-                turn = 0;
-                break;
+                state = 2;
+                RCLCPP_INFO(this->get_logger(), "I am in Left: '%s'", std::to_string(state));
+                turn += -1*rotation_step;
+            break;
+            
         }
 
         state_ = new_state;
@@ -138,50 +135,55 @@ private:
     
     void timer_callback(){
         
-        std_msgs::msg::Bool speed_msg;
+        std_msgs::msg::Int32 state_msg;
         std_msgs::msg::Int32 angle_msg; 
 
         // Set the message data based on the current state
-        speed_msg.data = forward;
-        angle_msg.data = turn;
+        state_msg.data = state;
+
 
         // Publish the commands
-        speed_publisher_->publish(speed_msg);
-
-        if (std::abs(angle_error_) <= angle_error_threshold_) {
-            angle_msg.data = turn;
-        } else {
-            angle_msg.data = 0; // Do not change the angle if error is too high
+        state_publisher_->publish(state_msg);
+        if(state == 2){
+            if (std::abs(relative_angle_ - turn) > std::abs(angle_threshold_)) {
+                
+            } else {
+                transitionToState(WheelchairState::Stopped);
+           
+            }
         }
-
+        angle_msg.data = turn;
         angle_publisher_->publish(angle_msg);
           
     }
 
-    rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr current_angle_subscriber_;
-    rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr angle_error_subscriber_;
-    rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr current_velocity_subscriber_;
-    rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr velocity_error_subscriber_;
+    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr current_angle_subscriber_;
+    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr angle_error_subscriber_;
+    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr relative_angle_subscriber_;
 
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr command_subscriber_;
 
-    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr speed_publisher_;
+    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr state_publisher_;
     rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr angle_publisher_;
 
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::TimerBase::SharedPtr speed_duration_timer_;
 
-    WheelchairState state_; 
-    bool forward;
+    WheelchairState state_;
+    int state; 
     int turn;    
     std::string command_;
 
     double current_angle_;
     double angle_error_;
-    double current_velocity_;
-    double velocity_error_;
-    const double angle_error_threshold_ = 1;
+
+    double relative_angle_;
+
+    const double angle_threshold_ = 1;
+
     bool is_forward_timer_active_;
+
+    const int rotation_step = 10;
 
     std::chrono::time_point<std::chrono::steady_clock> last_command_time_;
 };
