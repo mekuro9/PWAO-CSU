14b835e6eb54f6a21e40b5300865cce95c596380
mekuro9 <60172980+mekuro9@users.noreply.github.com>
2024-04-09 09:05:44 +0200

    Add files via upload

--- ros2web/flask-app/latency_test.py
+++ ros2web/flask-app/latency_test.py
@@ -1,8 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from datetime import datetime
 import requests
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/timestamp', methods=['GET'])
 def get_timestamp():
@@ -11,4 +16,4 @@ def get_timestamp():
     return jsonify({'timestamp': timestamp})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000, host='0.0.0.0')
