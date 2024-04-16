68dd226e2767d7a663b8e24336da46ec927bed82
mekuro9 <60172980+mekuro9@users.noreply.github.com>
2024-04-16 12:24:18 +0200

    Add files via upload

--- ros2web/flask-app/latency_test.py
+++ ros2web/flask-app/latency_test.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('rtt_msg.html')
 
 @app.route('/timestamp', methods=['GET'])
 def get_timestamp():
